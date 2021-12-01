9d424ed4bbe3ea77c4519669fb1922ff65907d1b
SparkleFox715 <jupiter.am705@gmail.com>
2021-12-01 07:36:08 -0600

    changes, pending at home

---
+++ network.py
@@ -0,0 +1,17 @@
+import socket
+import server
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server ="172.31.128.1"
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.id = self.connect()
+        print(self.id)
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+n = Network()
--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import socket
-import _thread
+from _thread import *
 import sys
 
-server = ""
+server = "172.31.128.1"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,8 +16,29 @@ except socket.error as e:
 s.listen(2)#number of people in netword
 print("Waiting for connection, Server started")
 
+
 def threaded_client(conn):
-    pass
+    conn.send(str.encode("Connected"))
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received:", reply)
+                print("Sending: ", reply)
+            conn.sendall(str.encode(reply))
+        except:
+            break
+    print("Lost connection")
+    conn.close()
 
 while True:
-    
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
+
+    start_new_thread(threaded_client, (conn, ))
